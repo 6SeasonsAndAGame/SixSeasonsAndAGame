48c449a720b49c048f053030b53c3c3b87764e67
sam <samwturner@gmail.com>
2020-11-06 18:44:22 -0500

    Deleted Wwise Plugin SDK from Git

--- Plugins/Wwise/Source/AkAudio/Classes/Platforms/AkPlatformBase.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "AkInclude.h"
-
-struct AKAUDIO_API FAkPlatformBase
-{
-	static FString GetWwisePluginDirectory();
-	static FString GetDSPPluginsDirectory(const FString& PlatformArchitecture);
-};
